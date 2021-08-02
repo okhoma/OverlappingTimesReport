c6f9e3aecc90d57828d33868423217876c1873ae
Oleh Khoma <oleh.khoma@eleks.com>
2021-08-02 19:39:48 +0300

    Modified version with business hours

--- report.py
+++ report.py
@@ -1,53 +1,80 @@
 from datetime import datetime as dt
 import argparse
 
+class OverlappingTimesReport:
+    def __init__(self, business_day_range = None):
+        self.business_day_range = business_day_range
 
-def ampm_to_minutes(ampm):
-    t = dt.strptime(ampm, "%I:%M%p")
-    return t.hour * 60 + t.minute
+    @staticmethod
+    def _ampm_to_minutes(ampm):
+        t = dt.strptime(ampm, "%I:%M%p")
+        return t.hour * 60 + t.minute
 
+    @staticmethod
+    def _minutes_to_ampm(m):
+        h = m//60
+        m = m%60
+        ampm = "am" if h < 12 else "pm"
+        if h == 0:
+            h = 12
+        elif h > 12:
+            h -= 12
+        return f"{h}:{m:02}{ampm}"
 
-def minutes_to_ampm(m):
-    h = m//60
-    m = m%60
-    ampm = "am" if h < 12 else "pm"
-    if h == 0:
-        h = 12
-    elif h > 12:
-        h -= 12
-    return f"{h}:{m:02}{ampm}"
+    def _eliminate_events_within_business_hours(self, times):
+        if self.business_day_range is None:
+            return times
 
-
-def get_overlapping_times(times):
-    num_times = len(times)
-    sorted_times = sorted(times)
-    for i in range(num_times):
-        start1, end1 = sorted_times[i]
-        
-        for j in range(i+1, num_times):
-            start2, end2 = sorted_times[j]
-            if start2 >= end1:
-                break
-            
-            if end2 > start1:
-                yield (start1, end1), (start2, end2)
-
-
-def format_overlapping_times(times):
-    if times:
-        t = minutes_to_ampm
-        for (start1, end1), (start2, end2) in times:
-            yield f"({t(start1)} - {t(end1)}) and ({t(start2)} - {t(end2)})"
+        result = []
+        bd_start, bd_end = self.business_day_range
+        for start, end in times:
+            if start >= bd_start and end <= bd_end:
+                result.append((start, end))
+        return result
 
 
-def read_times(file_name):
-    times = []
-    with open(file_name) as f:
-        _ = f.readline()  # skip header
-        for line in f:
-            start, end = line.split(",")
-            times.append((ampm_to_minutes(start), ampm_to_minutes(end.strip("\n"))))
-    return times
+    def _get_overlapping_times(self, times):
+        sorted_times = sorted(times)
+        remaining_times = self._eliminate_events_within_business_hours(sorted_times)
+        num_times = len(remaining_times)
+        if remaining_times:
+            for i in range(num_times):
+                start1, end1 = remaining_times[i]
+                
+                for j in range(i+1, num_times):
+                    start2, end2 = remaining_times[j]
+                    if start2 >= end1:
+                        break
+                    
+                    if end2 > start1:
+                        yield (start1, end1), (start2, end2)
+
+
+    def _format_overlapping_times(self, times):
+        if times:
+            t = self._minutes_to_ampm
+            for (start1, end1), (start2, end2) in times:
+                yield f"({t(start1)} - {t(end1)}) and ({t(start2)} - {t(end2)})"
+
+
+    def _read_times(self, file_name):
+        times = []
+        with open(file_name) as f:
+            _ = f.readline()  # skip header
+            for line in f:
+                start, end = line.split(",")
+                if start.startswith("#BDAY:"):
+                    start = start[6:]
+                    self.business_day_range = (self._ampm_to_minutes(start), self._ampm_to_minutes(end.strip("\n")))
+                else:
+                    times.append((self._ampm_to_minutes(start), self._ampm_to_minutes(end.strip("\n"))))
+        return times
+
+    def generate_report(self, file_name):
+        times = self._read_times(file_name)
+        overlapping = list(self._get_overlapping_times(times))
+        report = self._format_overlapping_times(overlapping) if overlapping else None
+        return report
 
 
 if __name__ == "__main__":
@@ -55,11 +82,13 @@ if __name__ == "__main__":
     parser.add_argument("file_name", help="File path with times")
     args = parser.parse_args()
 
-    times = read_times(args.file_name)
-    overlapping = list(get_overlapping_times(times))
+    report_generator = OverlappingTimesReport()
 
-    if overlapping:
-        report = format_overlapping_times(overlapping)
+#     _ = list(report_generator._get_overlapping_times([(5, 10), (1, 2), (3, 6), (6, 7), (7, 12), (15, 20)]))
+
+    report = report_generator.generate_report(args.file_name)
+
+    if report:
         print("Overlapping times:\n" + "\n".join(report))
     else:
         print("No overlapping times")
--- test_report.py
+++ test_report.py
@@ -1,7 +1,11 @@
 import pytest
 
-from report import ampm_to_minutes, minutes_to_ampm
-from report import get_overlapping_times, read_times, format_overlapping_times
+from report import OverlappingTimesReport
+
+@pytest.fixture
+def reporter():
+    r = OverlappingTimesReport()
+    return r
 
 @pytest.mark.parametrize("ampm,minutes", [
     ("12:00am", 0),
@@ -12,21 +16,35 @@ from report import get_overlapping_times, read_times, format_overlapping_times
     ("12:59pm", 12*60+59),
     ("9:30am", 9*60+30)
 ])
-def test_ampm_to_minutes(ampm, minutes):
-    assert ampm_to_minutes(ampm) == minutes
-    assert minutes_to_ampm(minutes) == ampm
+def test_ampm_to_minutes(reporter, ampm, minutes):
+    assert reporter._ampm_to_minutes(ampm) == minutes
+    assert reporter._minutes_to_ampm(minutes) == ampm
 
 @pytest.mark.parametrize("content,expected", [
     ("12:00am,12:00pm", [(0, 12*60)]),
     ("9:01am,9:02am\n10:11pm,11:59pm", [(9*60+1, 9*60+2), (22*60+11, 23*60+59)])
 ])
-def test_read_times(tmpdir, content, expected):
+def test_read_times(reporter, tmpdir, content, expected):
     file_name = tmpdir.join("times")
     with open(file_name, "w") as f:
         f.write("start,end\n")
         f.write(content)
-    actual = list(read_times(file_name))
+    actual = list(reporter._read_times(file_name))
     assert actual == expected
+
+
+@pytest.mark.parametrize("content,expected_business_day_range", [
+    ("12:00am,12:00pm", None),
+    ("#BDAY:9:00am,5:00pm\n9:00am,9:30pm", (9*60, 17*60))
+])
+def test_read_times_with_business_day_range(reporter, tmpdir, content, expected_business_day_range):
+    file_name = tmpdir.join("times")
+    with open(file_name, "w") as f:
+        f.write("start,end\n")
+        f.write(content)
+    _ = list(reporter._read_times(file_name))
+    
+    assert reporter.business_day_range == expected_business_day_range
 
 
 @pytest.mark.parametrize("times,overlapping", [
@@ -35,8 +53,18 @@ def test_read_times(tmpdir, content, expected):
     ([(5, 10), (1, 2), (3, 6), (6, 7), (7, 12), (15, 20)], 
      [((3, 6), (5, 10)), ((5, 10), (6, 7)), ((5, 10), (7, 12))]),
 ])
-def test_get_overlapping_times(times, overlapping):
-    assert list(get_overlapping_times(times)) == overlapping
+def test_get_overlapping_times(reporter, times, overlapping):
+    assert list(reporter._get_overlapping_times(times)) == overlapping
+
+@pytest.mark.parametrize("times,overlapping", [
+    ([(0, 1)], []),
+    ([(1, 2), (2, 3), (3, 4)], []),
+    ([(5, 10), (1, 2), (3, 6), (6, 7), (7, 12), (15, 20)], [((5, 10), (6, 7))]),
+])
+def test_get_overlapping_times_with_business_day(reporter, times, overlapping):
+    reporter.business_day_range = (4, 10)
+    assert list(reporter._get_overlapping_times(times)) == overlapping
+
 
 @pytest.mark.parametrize("overlapping,expected", [
     ([], []),
@@ -45,5 +73,5 @@ def test_get_overlapping_times(times, overlapping):
         ["(12:00am - 12:02am) and (12:01am - 12:03am)",
          "(12:05am - 12:10am) and (12:06am - 12:07am)"])
 ])
-def test_format_overlapping_times(overlapping, expected):
-    assert list(format_overlapping_times(overlapping)) == expected
+def test_format_overlapping_times(reporter, overlapping, expected):
+    assert list(reporter._format_overlapping_times(overlapping)) == expected
